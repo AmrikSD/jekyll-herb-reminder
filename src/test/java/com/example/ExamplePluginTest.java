179e0331aa15db73d21574277dd77884f267bdb0
Amrik Singh <Amrik.Singh@live.co.uk>
2022-08-19 20:21:11 +0100

    Create first version of plugin

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -2,12 +2,11 @@ package com.example;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import xyz.amrik.JekyllHerbReminderPlugin;
 
-public class ExamplePluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+public class ExamplePluginTest {
+	public static void main(String[] args) throws Exception {
+		ExternalPluginManager.loadBuiltin(JekyllHerbReminderPlugin.class);
 		RuneLite.main(args);
 	}
 }
